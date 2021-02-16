cf2d99e425a277ee15a1d301adff7cd32a5dfd09
Zac <zacharyflynn06@gmail.com>
2021-02-16 13:02:51 -0700

    change storage directory for carrierwave, all is well

--- app/models/photo_uploader.rb
+++ app/models/photo_uploader.rb
@@ -3,18 +3,18 @@ class PhotoUploader < CarrierWave::Uploader::Base
     storage :file
     
     def store_dir
-      "public/images"
+      "images"
     end
   
     def cache_dir
-      "public/images/tmp"
+      "images/tmp"
     end
   
-    # def extension_whitelist
-    #   %w(jpg jpeg gif png)
-    # end
+    def extension_whitelist
+      %w(jpg jpeg gif png)
+    end
   
-    # def content_type_whitelist
-    #   /image\//
-    # end
+    def content_type_whitelist
+      /image\//
+    end
 end
