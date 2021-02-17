f189d3ae14e786562ed05a17bd0c757a0ca74b22
Zac <zacharyflynn06@gmail.com>
2021-02-17 13:47:10 -0700

    fix typo on PostIngredients migration

--- db/migrate/20210211192019_create_post_ingredients.rb
+++ db/migrate/20210211192019_create_post_ingredients.rb
@@ -2,7 +2,7 @@ class CreatePostIngredients < ActiveRecord::Migration[5.2]
   def change
     create_join_table :posts, :ingredients do |t|
       t.index :post_id
-      t.index :category_id
+      t.index :ingredient_id
       
       t.integer :amount
       t.string :measurement_type
