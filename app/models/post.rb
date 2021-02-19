29e2e660f3b91a6972928041c5a181ff3959655e
Zac <zacharyflynn06@gmail.com>
2021-02-18 23:14:09 -0700

    add alex as user and his first post

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,8 @@ class Post < ActiveRecord::Base
     has_many :steps
     has_many :ingredient_posts
     has_many :ingredients, through: :ingredient_posts
-    # validates :title, presence: true, uniqueness: true
-    # validates :content, presence: true
+    validates :title, presence: true, uniqueness: true
+    validates :description, presence: true
     mount_uploader :avatar, PhotoUploader
     serialize :avatar, JSON
     
